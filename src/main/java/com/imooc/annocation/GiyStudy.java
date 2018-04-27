c6550676a8e859cdafc212a868eef25f938301bf
1770599952@qq.com <1770599952@qq.com>
2018-04-27 13:11:12 +0800

    test git

---
+++ src/main/java/com/imooc/annocation/GiyStudy.java
@@ -0,0 +1,9 @@
+package com.imooc.annocation;
+
+/**
+ * Created by MAIBENBEN on 2018/4/27.
+ */
+public class GiyStudy {
+
+    public void f(){}
+}
--- src/main/java/com/imooc/annocation/LogAction.java
+++ src/main/java/com/imooc/annocation/LogAction.java
@@ -12,4 +12,5 @@ import java.lang.annotation.Target;
 public @interface LogAction {
 	
     String value() default "";
+
 }
